d315f9e1ea62cc1aea2bb8a0fbb924c6b91091e6
Christopher Daniel <snugglesbear@users.noreply.github.com>
2025-03-07 15:39:42 -0600

    Starting day1

---
+++ Day1/day1.py
@@ -0,0 +1,6 @@
+# Thought process:
+# function to read in text file and split into two lists: left and right
+# fucntion to order the list in numerical order (doesn't matter if ascending or descending, as long as both lists are the same)
+# function to walk through through lists and find the distance between each index. Store the total distance
+
+main()
