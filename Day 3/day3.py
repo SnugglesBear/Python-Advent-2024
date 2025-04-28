69ccccdcbadd36c97904fbb1d367a4fccd845b35
Christopher Daniel <snugglesbear@users.noreply.github.com>
2025-04-28 15:13:49 -0500

    day 3 completed.

---
+++ Day 3/day3.py
@@ -0,0 +1,41 @@
+# scan for mul( and end at the next ) ?? maybe?
+# check to see if it follows mul(number,number) and discard the rest
+# do the actual math and add up what's remaining
+
+import re
+
+def extract_mul_calls(text):
+    # Use parentheses () inside the regex to capture the two numbers separately
+    pattern = r"(do\(\))|(don't\(\))|(mul\((\d+),(\d+)\))"
+    status = True
+    results = []
+    for match in re.finditer(pattern, text):
+        if match.group(1):  # do()
+            status = True
+            print("something was true")
+        elif match.group(2):  # dont()
+            status = False
+            print("something was false")
+        elif match.group(3):  # mul(a,b)
+            if status:
+
+                a = int(match.group(4))
+                b = int(match.group(5))
+                results.append(a * b)
+    return results
+
+def main():
+
+    with open("Python-Advent-2024/Day 3/input.txt", "r") as file:
+        content = file.read()
+
+    matches = extract_mul_calls(content)
+
+    print(len(matches))
+
+    total = sum(matches)
+
+    print(total)
+
+if __name__ == "__main__":
+    main()
